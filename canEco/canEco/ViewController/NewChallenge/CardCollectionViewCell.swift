f5b2f65058db1e822c3c0453ca35143bbd55f9a5
Marco Zulian <marco.ab.zulian@gmail.com>
2021-09-09 16:34:59 -0300

    Ajustes Card de desafio tela novo desafio

--- canEco/canEco/ViewController/NewChallenge/CardCollectionViewCell.swift
+++ canEco/canEco/ViewController/NewChallenge/CardCollectionViewCell.swift
@@ -12,15 +12,30 @@ class CardCollectionViewCell: UICollectionViewCell {
     let challengeImage = UIImageView()
     let challengeTitle = UILabel()
     let infoButton = UIButton()
-    
+
     let placeholderInformation = UILabel()
     
     let impactTitle = UILabel()
     
     let acceptChallengeButton = UIButton()
     
+    var challenge: Challenge? {
+        didSet {
+            setupLabels()
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        displayChallengeImage()
+        displayChallengeTitle()
+        displayInfoButton()
+        displayPlaceholderInformation()
+        displayImpactTitle()
+        displayAcceptChallengeButton()
+        
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 27
     }
     
     required init?(coder: NSCoder) {
@@ -34,10 +49,10 @@ class CardCollectionViewCell: UICollectionViewCell {
         challengeImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         challengeImage.centerYAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         challengeImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.8).isActive = true
-        challengeImage.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 232/129).isActive = true
+        challengeImage.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 129/232).isActive = true
         
-        challengeImage.backgroundColor = .systemBlue
-        challengeImage.clipsToBounds = false
+        challengeImage.backgroundColor = .lightGray
+//        challengeImage.clipsToBounds = false
     }
     
     func displayChallengeTitle() {
@@ -45,7 +60,7 @@ class CardCollectionViewCell: UICollectionViewCell {
         contentView.addSubview(challengeTitle)
         
         challengeTitle.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.7).isActive = true
-        challengeTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15).isActive = true
+        challengeTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.frame.width * 0.09).isActive = true
         challengeTitle.topAnchor.constraint(equalTo: challengeImage.bottomAnchor, constant: 10).isActive = true
         
         challengeTitle.textAlignment = .left
@@ -61,6 +76,9 @@ class CardCollectionViewCell: UICollectionViewCell {
         infoButton.centerYAnchor.constraint(equalTo: challengeTitle.centerYAnchor).isActive = true
         infoButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
         infoButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        infoButton.setImage(UIImage(systemName: "info.circle")?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 25, weight: .regular)), for: .normal)
+        infoButton.tintColor = .black
     }
     
     
@@ -74,6 +92,7 @@ class CardCollectionViewCell: UICollectionViewCell {
         
         placeholderInformation.numberOfLines = 0
         placeholderInformation.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        placeholderInformation.text = " 87 participantes       4 semanas"
         
         
     }
@@ -88,6 +107,7 @@ class CardCollectionViewCell: UICollectionViewCell {
         
         impactTitle.numberOfLines = 0
         impactTitle.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        impactTitle.text = "Impactos Positivos"
         
     }
     
@@ -95,13 +115,20 @@ class CardCollectionViewCell: UICollectionViewCell {
         acceptChallengeButton.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(acceptChallengeButton)
         
-        acceptChallengeButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.7).isActive = true
-        acceptChallengeButton.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 64/237).isActive = true
+        acceptChallengeButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.76).isActive = true
+        acceptChallengeButton.heightAnchor.constraint(equalTo: acceptChallengeButton.widthAnchor, multiplier: 64/237).isActive = true
         acceptChallengeButton.centerYAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         acceptChallengeButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        
+        acceptChallengeButton.setTitle("ACEITAR DESAFIO", for: .normal)
+        acceptChallengeButton.backgroundColor = UIColor(named: "lightGreen")
+        acceptChallengeButton.layer.cornerRadius = 16
     }
     
-    
+    func setupLabels() {
+        challengeTitle.text = challenge!.name
+        challengeImage.image = UIImage(named: "placeholder")
+    }
     
     
 }
--- canEco/canEco/ViewController/NewChallenge/NewChallengeViewController.swift
+++ canEco/canEco/ViewController/NewChallenge/NewChallengeViewController.swift
@@ -11,7 +11,8 @@ class NewChallengeViewController: UIViewController {
 
     var filter: UICollectionView!
     var data: [Category?] = [nil, Category.fashion, Category.food, Category.shopping, Category.transportation, Category.waste]
-    var benefit: NewChallengeCardCollectionViewCell!
+    var data2: [Challenge]!
+    var challengesCollectionView: UICollectionView!
     
     var collectionLayout: UICollectionViewFlowLayout! {
         didSet {
@@ -24,6 +25,8 @@ class NewChallengeViewController: UIViewController {
         
         let tabBar = UITabBarItem(title: "Novo Desafio", image: UIImage(systemName: "magnifyingglass"), selectedImage: UIImage(systemName: "magnifyingglass", withConfiguration: UIImage.SymbolConfiguration(weight: .bold)))
         tabBarItem = tabBar
+        
+        data2 = Challenges.getChallenges()
     }
     
     required init?(coder: NSCoder) {
@@ -33,8 +36,8 @@ class NewChallengeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setupFilter()
-        
+//        setupFilter()
+        displayChallengesCv()
 //        benefit = NewChallengeCardCollectionViewCell()
 //        benefit.challenge = RepeatableChallenge(name: "placeholder", category: .fashion, benefits: [.co2, .diseases, .energy], totalSteps: 10)
 //        benefit.translatesAutoresizingMaskIntoConstraints = false
@@ -67,21 +70,54 @@ class NewChallengeViewController: UIViewController {
         filter.delegate = self
         
     }
+    
+    func displayChallengesCv() {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        challengesCollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), collectionViewLayout: layout)
+        
+        challengesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(challengesCollectionView)
+        challengesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        challengesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        challengesCollectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        challengesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        challengesCollectionView.register(CardCollectionViewCell.self, forCellWithReuseIdentifier: "challengeCard")
+        challengesCollectionView.dataSource = self
+        challengesCollectionView.delegate = self
+        challengesCollectionView.backgroundColor = UIColor(named: "bege")
+//        challengesCollectionView.isPagingEnabled = true
+        challengesCollectionView.tag = 1
+    }
 
 }
 
 extension NewChallengeViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        data.count
+        if collectionView.tag == 1 {
+            return data2.count
+        }
+        return data.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView.tag == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "challengeCard", for: indexPath) as! CardCollectionViewCell
+            cell.challenge = data2[indexPath.item]
+            return cell
+        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "filterTag", for: indexPath) as! CategoryTagCollectionViewCell
         cell.category = data[indexPath.item]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if collectionView.tag == 1 {
+            return CGSize(width: 311, height: 475)
+        }
         
         let height = filter.frame.height
         let width: CGFloat!
@@ -95,6 +131,9 @@ extension NewChallengeViewController: UICollectionViewDataSource, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView.tag == 1 {
+            return
+        }
         let cell = collectionView.cellForItem(at: indexPath) as! CategoryTagCollectionViewCell
         
         if cell.displayMode == .fill {
