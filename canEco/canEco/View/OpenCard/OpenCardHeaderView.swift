34e9c0f70ebcc7f6069d4dfbdc07ee6ad3150fe9
Naomi <naomi.nknsh@gmail.com>
2021-09-13 16:06:11 -0300

    Changes open card header and switches to modal style

---
+++ canEco/canEco/View/OpenCard/OpenCardHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  OpenCardHeaderView.swift
+//  canEco
+//
+//  Created by Bruno Pastre on 13/09/21.
+//
+
+import UIKit
+
+class OpenCardHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
