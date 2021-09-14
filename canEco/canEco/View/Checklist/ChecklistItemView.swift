a7c3cf18ebe549978f8818f1a468e6f78480d6c0
Naomi <naomi.nknsh@gmail.com>
2021-09-14 17:06:56 -0300

    Starts ChecklistView

---
+++ canEco/canEco/View/Checklist/ChecklistItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItemView.swift
+//  canEco
+//
+//  Created by Bruno Pastre on 14/09/21.
+//
+
+import UIKit
+
+class ChecklistItemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
